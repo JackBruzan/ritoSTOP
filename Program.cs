285ef5d575d3c3c52ca14c28698d7cfd1b0beaa7
JackBruzan <jackbruzan@gmail.com>
2019-04-14 18:43:09 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace riot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game(2936546794);
+        }
+    }
+}
