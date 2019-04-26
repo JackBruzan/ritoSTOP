207480ddfae1cc44166850c566d9d7840cbacea9
JackBruzan <jackbruzan@gmail.com>
2019-04-25 20:45:41 -0500

    removed another old apiKey definition

--- Summoner.cs
+++ Summoner.cs
@@ -11,7 +11,7 @@ namespace riot
         {
             HttpClient client = new HttpClient();
             summonerName = summoner;
-            string apiKey = "RGAPI-7bdf5f7e-bc68-4741-b9a2-0d470ee3d62b";
+//            string apiKey = "RGAPI-7bdf5f7e-bc68-4741-b9a2-0d470ee3d62b";
 
             var t = client.GetAsync($"https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner}?api_key={API.apiKey}");
             dynamic summonerJson = JObject.Parse(t.Result.Content.ReadAsStringAsync().Result.ToString());
